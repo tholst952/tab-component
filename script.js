ac3aa017bddecab8adbd5cca7cdc475d8b44954e
trena <20079502+tholst952@users.noreply.github.com>
2023-10-31 14:35:04 -0700

    one active tab at a time is working. Now make a feature in the content match the color of the active tab

--- script.js
+++ script.js
@@ -7,28 +7,22 @@ const allContent = document.querySelectorAll('.content');
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.tab-btn');
   const tabNumber = clicked.dataset.tab;
-  console.log(tabNumber);
+  // console.log(tabNumber);
 
   //Guard clause
   if (!clicked) return;
 
   // remove the active classes from tabs & content
-  if (clicked)
-    tabs.forEach(tab => tab.classList.remove(`tab-btn-${tabNumber}-active`));
+  if (clicked) tabs.forEach(tab => tab.classList.remove(`active`));
   allContent.forEach(c => c.classList.remove(`content-active`));
 
   // activate tab
   clicked.classList.add(`tab-btn-${tabNumber}-active`);
 
   // add the active class to tab
-  document
-    .querySelector(`.tab-btn-${tabNumber}`)
-    .classList.add(`tab-btn-${tabNumber}-active`);
+  document.querySelector(`.tab-btn-${tabNumber}`).classList.add(`active`);
 
   // show the active tab's content
-
   const content = document.querySelector(`.content__${tabNumber}`);
   content.classList.add(`content-active`);
-  content.classList.remove(`content-hidden`);
-  // content.classList.add(`content__${tabNumber}`);
 });
